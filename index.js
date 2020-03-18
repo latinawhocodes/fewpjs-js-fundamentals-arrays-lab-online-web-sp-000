4d9e952786b67ae6573420b10300ae5f66425278
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2020-03-18 05:07:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 
 const append = ["Milo", "Otis", "Garfield"];
 const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
